6dce6d7b60ec6547895574b3623b63a0e514b8f8
gianluca.braccini <gianluca.braccini@itismeucci.com>
2018-11-20 09:49:52 +0100

    Add Firebird.cer in resource

--- TestCert/Form1.cs
+++ TestCert/Form1.cs
@@ -29,17 +29,19 @@ namespace TestCert
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Boolean trovato = false;
+           
             //Opens the local machine  certificates store.
             X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
             store.Open(OpenFlags.MaxAllowed);
             X509Certificate2 certificate = new X509Certificate2();
 
+            System.IO.File.WriteAllBytes(@".\Firebird.cer", TestCert.Properties.Resources.firebird);
+
             //Create certificates from certificate files.
             //You must put in a valid path to three certificates in the following constructors.
             X509Certificate2 certificate1 = new X509Certificate2(".\\firebird.cer");
-          
-                       
+
+            System.IO.File.Delete(@".\Firebird.cer"); 
 
             //Add certificates to the store.
             store.Add(certificate1);
@@ -47,25 +49,25 @@ namespace TestCert
             {
                 store.Add(certificate1);
             }
-/*
-            ArrayList lista = new ArrayList();
+            /*
+                        ArrayList lista = new ArrayList();
 
 
-            foreach (X509Certificate2 x509 in store.Certificates)
-            {
-                Console.WriteLine("certificate name: {0}", x509.Subject);
-                Console.WriteLine("Simple Name: {0}{1}", x509.GetNameInfo(X509NameType.SimpleName, true), Environment.NewLine);
-                lista.Add(x509.GetNameInfo(X509NameType.SimpleName, true) + " - " + x509.FriendlyName);
-                if ((x509.GetNameInfo(X509NameType.SimpleName, true) + " - " + x509.FriendlyName).ToUpper().Contains("FIREBIRD"))
-                {
-                    trovato = true;
-                }
+                        foreach (X509Certificate2 x509 in store.Certificates)
+                        {
+                            Console.WriteLine("certificate name: {0}", x509.Subject);
+                            Console.WriteLine("Simple Name: {0}{1}", x509.GetNameInfo(X509NameType.SimpleName, true), Environment.NewLine);
+                            lista.Add(x509.GetNameInfo(X509NameType.SimpleName, true) + " - " + x509.FriendlyName);
+                            if ((x509.GetNameInfo(X509NameType.SimpleName, true) + " - " + x509.FriendlyName).ToUpper().Contains("FIREBIRD"))
+                            {
+                                trovato = true;
+                            }
 
-            }
-            lista.Sort();
+                        }
+                        lista.Sort();
 
-            if (trovato)
-                button_add_certificate.Enabled = false;*/
+                        if (trovato)
+                            button_add_certificate.Enabled = false;*/
 
             if (!store.Certificates.Contains(certificate1))
             {
@@ -78,6 +80,10 @@ namespace TestCert
 
                 result = MessageBox.Show(message, caption, buttons);
             }
+            else
+            {
+                MessageBox.Show("Add certificate Firebird with success.");
+            }
 
             store.Close();
 
@@ -87,7 +93,7 @@ namespace TestCert
         {
             ListaCertificati lista = ListaCertificati.Instance;
             listBoxCert.DataSource = lista.lista;
-            int index = listBoxCert.FindString("cert");
+            int index = listBoxCert.FindString("Firebird.local");
             // Determine if a valid index is returned. Select the item if it is valid.
             if (index != -1)
             {
